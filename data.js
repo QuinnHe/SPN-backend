5cd953602ee100b7d44098f038b576e43f7b9518
Zoetic-Zephyr <31701275+Zoetic-Zephyr@users.noreply.github.com>
2019-07-29 17:31:31 +0800

    Change meter data to [[LON, LAT], avail, price, pref, partner]

--- data.js
+++ data.js
@@ -4,18 +4,20 @@ const csv = require('fast-csv');
 const p1 = new Promise(function readCsv(resolve, reject) {
     let csvData = {};
     fs.createReadStream('./meterLoc.csv')
-    .pipe(csv.parse())
-    .on('error', (error) => {
-        reject(error);
-    })
-    .on('data', (data) => {
-        // csv[meterID] = [LON, LAT, avail, price, pref, partner ...]
-        csvData[data[0]] = [data[1], data[2], Math.round(10 * Math.random()), Math.round(20 * Math.random()), 5, -1];
-    })
-    .on('end', (rowCount) => {
-        console.log('Parsing complete, read', rowCount, 'records.');
-        resolve(csvData);
-    });
+        .pipe(csv.parse())
+        .on('error', (error) => {
+            reject(error);
+        })
+        .on('data', (data) => {
+            // csvData[meterID] = [[LON, LAT], avail, price, pref, partner ...]
+            csvData[data[0]] = [
+                [data[1], data[2]], Math.round(10 * Math.random()), Math.round(20 * Math.random()), 5, -1
+            ];
+        })
+        .on('end', (rowCount) => {
+            console.log('Parsing complete, read', rowCount, 'records.');
+            resolve(csvData);
+        });
 });
 
 module.exports.csvDataPromise = p1;
